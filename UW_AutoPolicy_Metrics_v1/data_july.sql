b13769de257f607ba64fb14a4e9b786d14174771
peteralonzo <peter.alonzo@thehartford.com>
2024-11-06 08:13:20 -0500

    Added comments to final query

--- UW_AutoPolicy_Metrics_v1/data_july.sql
+++ UW_AutoPolicy_Metrics_v1/data_july.sql
@@ -32,13 +32,14 @@ SUM(AUTO_OP_LOSS.GLASS_TOW_INCUR_CLM_CNT) AS GLASS_TOW_INCUR_CLM_CNT,
 SUM(AUTO_OP_LOSS.GLASS_TOW_INCUR_LOSS_AMT) AS GLASS_TOW_INCUR_LOSS_AMT,
 
 SUM(DERIV_ADJ_EP_AMT) AS EP,                                 -- Earned Premium
-DAY(LAST_DAY(TO_DATE(EARNED_MONTH || '01', 'YYYYMMDD'))) / 
-IFF(MOD(SUBSTR('202407', 1, 4), 4) = 0 AND (MOD(SUBSTR('202407', 1, 4), 100) != 0 OR MOD(SUBSTR('202407', 1, 4), 400) = 0), 366, 365) AS EE
+DAY(LAST_DAY(TO_DATE(EARNED_MONTH || '01', 'YYYYMMDD'))) /   -- Calculates # of days in Month
+IFF(MOD(SUBSTR('202407', 1, 4), 4) = 0 AND (MOD(SUBSTR('202407', 1, 4), 100) != 0 OR MOD(SUBSTR('202407', 1, 4), 400) = 0), 366, 365) -- Calculates # of days in years
+AS EE                                                        -- Earned Exposure calculation
 FROM "DSC_PLBI_DB"."APP_AUTO_PRD"."AUTO_OPERATIONAL_LOSS" AS AUTO_OP_LOSS
 JOIN USER_DB.AD1_PA08042.UNDERWRITING_AUTO_POLICIES AS UW_AUTO
 ON AUTO_OP_LOSS.POL_ID = UW_AUTO.POL_ID
 WHERE ECLC_COV_TYPE_ABBR_GRP != 'PUP'
-AND EARNED_MONTH >= '202407'
+AND EARNED_MONTH >= '202407'                  -- Filter for only showing data past July 2024
 GROUP BY UW_AUTO.POL_ID, EARNED_MONTH         -- Grouped by Policy ID and Earned Month
 ORDER BY UW_AUTO.POL_ID, EARNED_MONTH         -- Ordered by Policy ID and Earned Month
 )
