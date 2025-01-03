849795cdc5c6443c6e200256dc1918d15129e48a
peteralonzo <peter.alonzo@thehartford.com>
2025-01-03 20:11:57 +0000

    Fixed E0 Queries

--- Renewal_Zone_Retention/E0_Persistency.sql
+++ Renewal_Zone_Retention/E0_Persistency.sql
@@ -13,19 +13,31 @@ in the next 60 days */
 rz_policies AS
 (
 SELECT * FROM labeled_ap 
-WHERE auto_rn = 1 
+WHERE auto_rn = 1
 AND POL_ID IN (SELECT CAST(POL_ID AS VARCHAR) FROM USER_DB.AD1_PA08042.RENEWAL_ZONE)
 AND ((DATEADD(DAY, -60, POL_EXP_DT) BETWEEN '2024-10-01' AND CURRENT_DATE) 
-OR POL_EXP_DT > DATEADD(DAY, 60, CURRENT_DATE))
+OR POL_EXP_DT > CURRENT_DATE)
 )
 
-/* Used for Decision Breakdown 
-Make sure E+0 SELECT statement below is commented out before running*/
-SELECT CASE WHEN TRANS_TYP_DESC IN ('Cancel', 'Renew') THEN TRANS_TYP_DESC 
-ELSE 'Other' END AS TRANS_DECISION,
-COUNT(DISTINCT POL_ID)
+/* Used for Current Policy Status
+Make sure two queries below are commented out before running */
+SELECT CASE 
+WHEN ATR_E_FLAT = 1 AND CANCEL_E_FLAT = 0 THEN 'Active'
+WHEN ATR_E_FLAT = 1 AND CANCEL_E_FLAT = 1 THEN 'Cancel'
+WHEN ATR_E_FLAT = 0 AND CANCEL_E_FLAT = 0 AND CANCEL_FLAG = 1 THEN 'Cancel'
+ELSE 'Pending' END AS STATUS,
+COUNT(DISTINCT POL_ID) AS CNT
 FROM rz_policies
-GROUP BY TRANS_DECISION
+GROUP BY STATUS
+
+/* Used for E+0 Policy Count
+Make sure Current Policy Status and E+0 calculation queries
+are commented out before running */
+SELECT CANCEL_E_FLAT, COUNT(DISTINCT POL_ID) AS CNT
+FROM rz_policies
+WHERE ATR_E_FLAT = 1
+GROUP BY CANCEL_E_FLAT
+ORDER BY CANCEL_E_FLAT
 
 /* Used for E+0 calculation 
 Make sure Decision Breakdown SELECT statement above is commented out before running*/
@@ -53,25 +65,37 @@ full_book AS
 (
 SELECT * FROM labeled_ap 
 WHERE auto_rn = 1 
-AND ((DATEADD(DAY, -60, POL_EXP_DT) BETWEEN '2024-10-01' AND CURRENT_DATE) 
-OR POL_EXP_DT > DATEADD(DAY, 60, CURRENT_DATE))
+AND ((DATEADD(DAY, -60, POL_EXP_DT) BETWEEN '2024-10-01' AND CURRENT_DATE)
+OR POL_EXP_DT > CURRENT_DATE)
 )
 
-/* Used for Decision Breakdown 
-Make sure E+0 SELECT statement below is commented out before running*/
-SELECT CASE WHEN TRANS_TYP_DESC IN ('Cancel', 'Renew') THEN TRANS_TYP_DESC 
-ELSE 'Other' 
-END AS TRANS_DECISION,
-COUNT(DISTINCT POL_ID)
+/* Used for Current Policy Status
+Make sure two queries below are commented out before running */
+SELECT CASE 
+WHEN ATR_E_FLAT = 1 AND CANCEL_E_FLAT = 0 THEN 'Active'
+WHEN ATR_E_FLAT = 1 AND CANCEL_E_FLAT = 1 THEN 'Cancel'
+WHEN ATR_E_FLAT = 0 AND CANCEL_E_FLAT = 0 AND CANCEL_FLAG = 1 THEN 'Cancel'
+ELSE 'Pending' END AS STATUS,
+COUNT(DISTINCT POL_ID) AS CNT
 FROM full_book
-GROUP BY TRANS_DECISION
+GROUP BY STATUS
+
+/* Used for E+0 Policy Count
+Make sure Current Policy Status and E+0 calculation queries
+are commented out before running */
+SELECT CANCEL_E_FLAT, 
+COUNT(DISTINCT POL_ID) AS CNT
+FROM full_book
+WHERE ATR_E_FLAT = 1
+GROUP BY CANCEL_E_FLAT
+ORDER BY CANCEL_E_FLAT
 
 /* Used for E+0 calculation 
 Make sure Decision Breakdown SELECT statement above is commented out before running*/
 SELECT 
 CONCAT(ROUND(((CASE WHEN sum(ATR_E_FLAT) IS NOT NULL THEN sum(ATR_E_FLAT) ELSE 0 END - 
 CASE WHEN sum(CANCEL_E_FLAT) IS NOT NULL THEN sum(CANCEL_E_FLAT) ELSE 0 END) / 
-NULLIF(CASE WHEN sum(ATR_E_FLAT) IS NOT NULL THEN sum(ATR_E_FLAT) ELSE 0 END,0.0)) * 100, 2), '%') AS "E+0" 
+NULLIF(CASE WHEN sum(ATR_E_FLAT) IS NOT NULL THEN sum(ATR_E_FLAT) ELSE 0 END,0.0)) * 100, 2), '%') AS "E+0"
 FROM full_book
 
 -------------------------------------------------------------------------------------------------------------------------------------------
@@ -94,21 +118,33 @@ SELECT * FROM labeled_ap
 WHERE auto_rn = 1 
 AND POL_ID NOT IN (SELECT CAST(POL_ID AS VARCHAR) FROM USER_DB.AD1_PA08042.RENEWAL_ZONE)
 AND ((DATEADD(DAY, -60, POL_EXP_DT) BETWEEN '2024-10-01' AND CURRENT_DATE) 
-OR POL_EXP_DT > DATEADD(DAY, 60, CURRENT_DATE))
+OR POL_EXP_DT > CURRENT_DATE)
 )
 
-/* Used for Decision Breakdown 
-Make sure E+0 SELECT statement below is commented out before running*/
-SELECT CASE WHEN TRANS_TYP_DESC IN ('Cancel', 'Renew') THEN TRANS_TYP_DESC 
-ELSE 'Other' END AS TRANS_DECISION,
-COUNT(DISTINCT POL_ID)
+/* Used for Current Policy Status
+Make sure two queries below are commented out before running */
+SELECT CASE 
+WHEN ATR_E_FLAT = 1 AND CANCEL_E_FLAT = 0 THEN 'Active'
+WHEN ATR_E_FLAT = 1 AND CANCEL_E_FLAT = 1 THEN 'Cancel'
+WHEN ATR_E_FLAT = 0 AND CANCEL_E_FLAT = 0 AND CANCEL_FLAG = 1 THEN 'Cancel'
+ELSE 'Pending' END AS STATUS,
+COUNT(DISTINCT POL_ID) AS CNT
 FROM exclude_rz
-GROUP BY TRANS_DECISION
+GROUP BY STATUS
+
+/* Used for E+0 Policy Count
+Make sure Current Policy Status and E+0 calculation queries
+are commented out before running */
+SELECT CANCEL_E_FLAT, COUNT(DISTINCT POL_ID) AS CNT
+FROM exclude_rz
+WHERE ATR_E_FLAT = 1
+GROUP BY CANCEL_E_FLAT
+ORDER BY CANCEL_E_FLAT
 
 /* Used for E+0 calculation 
 Make sure Decision Breakdown SELECT statement above is commented out before running*/
 SELECT 
 CONCAT(ROUND(((CASE WHEN sum(ATR_E_FLAT) IS NOT NULL THEN sum(ATR_E_FLAT) ELSE 0 END - 
 CASE WHEN sum(CANCEL_E_FLAT) IS NOT NULL THEN sum(CANCEL_E_FLAT) ELSE 0 END) / 
-NULLIF(CASE WHEN sum(ATR_E_FLAT) IS NOT NULL THEN sum(ATR_E_FLAT) ELSE 0 END,0.0)) * 100, 2), '%') AS "E+0", 
+NULLIF(CASE WHEN sum(ATR_E_FLAT) IS NOT NULL THEN sum(ATR_E_FLAT) ELSE 0 END,0.0)) * 100, 2), '%') AS "E+0"
 FROM exclude_rz
